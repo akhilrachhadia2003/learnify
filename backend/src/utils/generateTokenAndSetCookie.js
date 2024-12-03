0d2e176027987d5875eb9b302747fc69dd02a46f
akhilrachhadia2003 <akrac311@gmail.com>
2024-12-03 13:01:35 +0530

    Delete backend directory

--- backend/src/utils/generateTokenAndSetCookie.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateTokenAndSetCookie = (res, userId) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-		expiresIn: "1d",
-	});
-	return token;
-}; 
