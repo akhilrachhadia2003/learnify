2132a2852a779d61d13de7e3f9acee2abe68e0a9
akhilrachhadia2003 <akrac311@gmail.com>
2024-12-04 19:02:23 +0530

    Delete client directory

--- client/src/hooks/TokenDecoder.js
+++
@@ -1,8 +0,0 @@
-import {jwtDecode} from 'jwt-decode';
-
-const getUserByToken = (token) => {
-    if (!token) return null;
-    const decodedToken = jwtDecode(token);
-    return decodedToken.userId;
-}
-export default getUserByToken;
